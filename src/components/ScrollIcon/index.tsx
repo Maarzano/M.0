665d35d0e1b6a01786f0b3fc5e7017b696791676
Marzano <arthuramarzano10@gmail.com>
2025-11-05 16:37:50 -0300

    feat(ScrollIcon) adiciona ScrollIcon com seu estilo e tudo mais

---
+++ src/components/ScrollIcon/index.tsx
@@ -0,0 +1,18 @@
+import { StyledWrapper } from './styles'
+
+const ScrollIcon = () => {
+  return (
+    <StyledWrapper>
+      <div className="scrolldown">
+        <div className="chevrons">
+          <div className="chevrondown" />
+          <div className="chevrondown" />
+        </div>
+      </div>
+    </StyledWrapper>
+  );
+}
+
+
+
+export default ScrollIcon;
