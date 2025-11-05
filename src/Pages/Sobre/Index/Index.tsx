13242bb7e59d85c16585f9d655c7a268aa197be0
Marzano <arthuramarzano10@gmail.com>
2025-11-05 19:16:01 -0300

    fix(nome de import) corrige nome de import c -> C

--- src/Pages/Sobre/Index/Index.tsx
+++ src/Pages/Sobre/Index/Index.tsx
@@ -1,4 +1,4 @@
-import ScrollIcon from "../../../components/ScrollIcon";
+import ScrollIcon from "../../../Components/ScrollIcon";
 import { Separação, Wrapper } from "./styles";
 
 const Index = () => {
