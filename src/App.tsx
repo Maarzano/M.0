772f4fc3405157c8d5e3b3264fa50d28f383b361
Marzano <arthuramarzano10@gmail.com>
2025-11-06 13:22:19 -0300

    feat(LinhaLateral) começa a criar a linha, a ideia

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { defaultTheme } from "./styles/theme"
 import { GlobalStyle } from "./styles/GlobalStyle"
 
 import Index from "./Pages/Sobre/Index/Index" 
+import Linha from "./Components/Linha"
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
+      <Linha/>
       <Index/>
     </ThemeProvider>
   )
