69c306ab11113a6cf3cf7d972a2805c2a6d852a8
Marzano <arthuramarzano10@gmail.com>
2025-11-20 16:50:55 -0300

    feat(container && <FraseDia/>) adiciona container em volta de toda a página para ter um tamanho máximo e adiciona a sessão FraseDia ao App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import { ThemeProvider } from "styled-components"
+import styled, { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/theme"
 import { GlobalStyle } from "./styles/GlobalStyle"
 
 import Index from "./Pages/Sobre/Index/Index" 
+import FraseDia from "./Pages/Sobre/FraseDia"
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Index/>
+      <Wrapper>
+        <Index/>
+        <FraseDia/>
+      </Wrapper>
     </ThemeProvider>
   )
 }
+
+export const Wrapper = styled.div`
+  width: 95vw;
+  margin: auto;
+`
 
 export default App
