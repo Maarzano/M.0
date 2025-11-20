3942d63088fb49610a4c7fb311fb1deadd2c2d46
Marzano <arthuramarzano10@gmail.com>
2025-11-20 17:02:20 -0300

    feat(FraseDia) cria e configura o componente FraseDia

--- src/Pages/Sobre/FraseDia/index.tsx
+++ src/Pages/Sobre/FraseDia/index.tsx
@@ -1,4 +1,18 @@
+import ScrollReveal from "../../../Components/ScrollReveal";
+import { Wrapper } from "./styles";
+
 const FraseDia = () => {
+    return (
+        <Wrapper>
+            <div>
+                <ScrollReveal fontSize="10rem">
+                    {/*Essa frase virá de API, é apenas temporário */}
+                        Coding is the art of building worlds from logic, passion, and imagination.
+                </ScrollReveal>
+            </div>
+        </Wrapper>
+    )
+    
 
 }
 
