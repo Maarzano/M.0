74c40c74d32e2d963b946a46f0a28a5d490a2bcc
Marzano <arthuramarzano10@gmail.com>
2025-11-20 16:52:54 -0300

    refactor(styles.css -> styles.ts) muda de css para styled components para podermos mudar tamanho de fonte e outras coisas se quisermos no lado de fora

---
+++ src/components/ScrollReveal/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const StyledH2 = styled.h2.attrs(props => ({
+  className: `scroll-reveal ${props.className}`
+}))`
+  margin: 20px 0;
+  
+  .word {
+    display: inline-block;
+  }
+`;
+
+export const StyledP = styled.p.attrs(props => ({
+  className: `scroll-reveal-text ${props.className}`
+}))<{ size: string }>`
+  font-size: ${props => props.size};
+  line-height: 1.5;
+  font-weight: 600;
+`;
