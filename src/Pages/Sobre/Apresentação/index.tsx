102d0599213adf936257a4102729943f6f1285ce
Marzano <arthuramarzano10@gmail.com>
2025-11-20 18:36:56 -0300

    feat(Sobre/Apresentação) adiciona sessõa de apresentação já com estilização e utilizando o componente ScrollReveal

---
+++ src/Pages/Sobre/Apresentação/index.tsx
@@ -0,0 +1,15 @@
+import ScrollReveal from "../../../components/ScrollReveal";
+import { Wrapper } from "./styles";
+
+const Apresentação = () => {
+    return (
+        <Wrapper>
+            <div>
+                <ScrollReveal fontSize="5rem" textAlign="center">
+                    Olá! Sou Arthur Marzano, desenvolvedor Full-Stack apaixonado por construir aplicações escaláveis e eficientes, com atenção a Clean Code, testes automatizados e boas práticas. Tenho facilidade para aprender novas tecnologias rapidamente e meu objetivo é desenvolver softwares que resolvam problemas reais e impactem positivamente negócios e usuários.
+                </ScrollReveal>
+            </div>
+        </Wrapper>
+    )
+}
+export default Apresentação;
---
+++ src/Pages/Sobre/Apresentação/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    div {
+        margin: 0rem 19rem;
+    }
+`;
