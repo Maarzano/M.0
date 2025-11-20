303bed469bd2a4495c28771508f1e2417895c8bc
Marzano <arthuramarzano10@gmail.com>
2025-11-20 16:51:41 -0300

    fix(styles do sobre) muda de 100vh para 100% pois estava ultrapassando e criando scroll em baixo

--- src/Pages/Sobre/Index/styles.ts
+++ src/Pages/Sobre/Index/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
     display: flex;
-    min-width: 100vw;
+    min-width: 100%;
     min-height: 100vh;
     position: relative
 `
