75b3ba5484f85b04e33ba0d694d0fdd55c803b49
Marzano <arthuramarzano10@gmail.com>
2025-11-20 17:01:56 -0300

    feat(styles.ts do FraseDIa) cria Wrapper para deixar a frase no meio da tela co mmargem

---
+++ src/Pages/Sobre/FraseDia/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    div {
+        margin: 0.3rem;
+    }
+`;
