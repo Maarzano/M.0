8e49cbc8ceb2dccfa84b81361b7d2c370e7cac13
Marzano <arthuramarzano10@gmail.com>
2025-11-21 12:29:05 -0300

    feat(ScrollReveal(+quotes)) adiciona quotes opcionais no styles.ts do ScrollReveal

--- src/components/ScrollReveal/styles.ts
+++ src/components/ScrollReveal/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const StyledH2 = styled.h2.attrs(props => ({
   className: `scroll-reveal ${props.className}`
@@ -12,9 +12,39 @@ export const StyledH2 = styled.h2.attrs(props => ({
 
 export const StyledP = styled.p.attrs(props => ({
   className: `scroll-reveal-text ${props.className}`
-}))<{ size: string, textAlign: string }>`
+}))<{ size: string; textAlign: string; $showQuotes?: boolean }>`
   font-size: ${props => props.size};
   line-height: 1.5;
   font-weight: 600;
-  text-align: ${props => props.textAlign}
+  text-align: ${props => props.textAlign};
+  
+  position: relative;
+  display: inline-block; 
+  padding: 0.2em 0.6em;
+
+  ${(props) => props.$showQuotes && css`
+    &::before,
+    &::after {
+        content: "";
+        position: absolute;
+        width: 0.4em;
+        height: 0.4em; 
+        background-color: #9F35FF;
+        
+        mask-image: url("/public/svg/quote.svg");
+        mask-repeat: no-repeat;
+        mask-size: contain;
+    }
+
+    &::before {
+        top: -0.1em;
+        left: -0.1em;
+    }
+
+    &::after {
+        bottom: -0.1em;
+        right: -0.1em;
+        transform: rotate(180deg);
+    }
+  `}
 `;
