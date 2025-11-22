0448128f1016e077801bf8964939e6defba2eeec
Marzano <arthuramarzano10@gmail.com>
2025-11-22 17:50:10 -0300

    fix(import( /public/svg/quote.svg -> /svg/quote.svg)) muda import de svg quote no componente de ScrollReveal para o certo

--- src/components/ScrollReveal/styles.ts
+++ src/components/ScrollReveal/styles.ts
@@ -35,7 +35,7 @@ export const StyledP = styled.p.attrs(props => ({
         
         background-color: ${props => props.theme.colors.primary};
         
-        mask-image: url("/public/svg/quote.svg");
+        mask-image: url("/svg/quote.svg");
         mask-repeat: no-repeat;
         mask-size: contain;
     }
