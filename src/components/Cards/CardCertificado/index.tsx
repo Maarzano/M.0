2f39065ea7073785b1228fab4083ccea31784757
maarzano <arthuramarzano10@gmail.com>
2025-11-24 20:18:58 -0300

    styles(CardCertificado) coloca o título no meio do card, centralizado apenas temporariamente para testar

--- src/components/Cards/CardCertificado/index.tsx
+++ src/components/Cards/CardCertificado/index.tsx
@@ -24,6 +24,7 @@ const CardCertificado: React.FC<CardCertificadoProps> = ({
 }) => {
     return (
         <Wrapper className="cursor-target">
+            {titulo}
         </Wrapper>
     );
 
--- src/components/Cards/CardCertificado/styles.ts
+++ src/components/Cards/CardCertificado/styles.ts
@@ -4,5 +4,9 @@ export const Wrapper = styled.div`
     width: 50rem;
     height: 50rem;
     border-radius: 10%;
-    background-color: ${props => props.theme.colors.background2}
+    background-color: ${props => props.theme.colors.background2};
+    //temporário:
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `
