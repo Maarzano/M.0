0da62359c8d541558a78df7cb40770d7845aa7af
maarzano <arthuramarzano10@gmail.com>
2025-11-24 19:55:48 -0300

    feat(CardCertificado(styles.ts)) começa a criação do arquivo visual do componente CardCertificado, com tamanho para teste.

---
+++ src/components/Cards/CardCertificado/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    width: 50rem;
+    height: 50rem;
+    border-radius: 10%;
+    background-color: ${props => props.theme.colors.background2}
+`
