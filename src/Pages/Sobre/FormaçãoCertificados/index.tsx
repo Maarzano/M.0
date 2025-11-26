ff0c71978fceceafdca7152d10108bb3e5798ad1
Marzano <arthuramarzano10@gmail.com>
2025-11-26 17:11:35 -0300

    feat(FormaçãoCertificados) coloca algumas informações no card para testar as aparições no navegador

--- src/Pages/Sobre/FormaçãoCertificados/index.tsx
+++ src/Pages/Sobre/FormaçãoCertificados/index.tsx
@@ -6,12 +6,14 @@ const FormacaoCertificado = () => {
     return (
         <Wrapper>
             <CardCertificado 
-                titulo={"TESTE"} 
-                dataInicio={new Date} 
-                modalidade={EModalidadeCurso.ONLINE} 
-                descricao={""} 
-                tecnologias={[]}
-            >
+                img={{imgSrc: "/public/assets/imgs/unaIconQuadradovermelho.png", background: "#E60026"}}
+                titulo={"Ciência da Computação"}
+                dataInicio={new Date("05/21/2025")}
+                modalidade={EModalidadeCurso.PRESENCIAL}
+                localizacao="UNA - Aimorés"
+                descricao="Formação acadêmica robusta em Ciência da Computação com ênfase no desenvolvimento e gestão de soluções tecnológicas inovadoras. O currículo forneceu uma base sólida em Engenharia de Software, Estrutura de Dados e Algoritmos..."
+                tecnologias={[]} >
+                
             </CardCertificado>
         </Wrapper>
     );
