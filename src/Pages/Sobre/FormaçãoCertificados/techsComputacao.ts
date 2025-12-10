788bf6bcbe3a0bc407aeb3374505f771888fd036
Marzano <arthuramarzano10@gmail.com>
2025-12-10 20:22:04 -0300

    fix(techsComputacao(MySQL)) muda o nome de SQL para MySQL

--- src/Pages/Sobre/FormaçãoCertificados/techsComputacao.ts
+++ src/Pages/Sobre/FormaçãoCertificados/techsComputacao.ts
@@ -4,6 +4,6 @@ export const techsComputacao: ITecnologia[] = [
         { nome: "Java", img: "/public/assets/svg/java.svg", link: "https://www.java.com" },
         { nome: "C++", img: "/public/assets/svg/cpp.svg" },
         { nome: "Python", img: "/public/assets/svg/python.svg" },
-        { nome: "SQL", img: "/public/assets/svg/mysql-icon-light.svg" },
+        { nome: "MySQL", img: "/public/assets/svg/mysql-icon-light.svg" },
         { nome: "Git", img: "/public/assets/svg/git.svg" }
     ];
