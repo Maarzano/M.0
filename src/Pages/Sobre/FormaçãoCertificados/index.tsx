05e09a90a62875fb828073abcc1a57b95d9c5f88
Marzano <arthuramarzano10@gmail.com>
2025-12-15 17:51:59 -0300

    change(Page(FormacaoCertificado(CardCertificado -> CertificateList))) troca um antigo exemplo de card para um componente a acesso a dados para trazer os cards dinamicamente

--- src/Pages/Sobre/FormaçãoCertificados/index.tsx
+++ src/Pages/Sobre/FormaçãoCertificados/index.tsx
@@ -1,21 +1,10 @@
-import CardCertificado from "../../../components/Cards/CardCertificado";
-import { EModalidadeCurso } from "../../../Types/EModalidadeCurso";
+import CertificateList from "../../../components/Lists/CertificateList";
 import { Wrapper } from "./styles";
-import { techsComputacao } from "./techsComputacao";
 
 const FormacaoCertificado = () => {
     return (
         <Wrapper>
-            <CardCertificado 
-                img={{imgSrc: "/public/assets/imgs/unaIconQuadradovermelho.png", background: "#E60026"}}
-                titulo={"Ciência da Computação"}
-                dataInicio={new Date("05/21/2025")}
-                modalidade={EModalidadeCurso.PRESENCIAL}
-                localizacao="UNA - Aimorés"
-                descricao="Formação acadêmica robusta em Ciência da Computação com ênfase no desenvolvimento e gestão de soluções tecnológicas inovadoras. O currículo forneceu uma base sólida em Engenharia de Software, Estrutura de Dados e Algoritmos..."
-                tecnologias={techsComputacao} >
-                
-            </CardCertificado>
+            <CertificateList/>
         </Wrapper>
     );
 }
