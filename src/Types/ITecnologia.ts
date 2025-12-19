dc91500455ba754f587e12baea6dd1691f392223
maarzano <arthuramarzano10@gmail.com>
2025-12-18 23:14:38 -0300

    refactor(ITecnologia) muda o tipo do nome (string -> NomeTecnologia) assim são apenas possíveis nomes determinados pelo arquivo NOME_TECNOLOGIAS

--- src/Types/ITecnologia.ts
+++ src/Types/ITecnologia.ts
@@ -1,5 +1,7 @@
+import type { NomeTecnologia } from "../constants/NOME_TECNOLOGIAS";
+
 export interface ITecnologia {
-    nome: string,
+    nome: NomeTecnologia,
     descricao?: string,
     img: string,
     link?: string
