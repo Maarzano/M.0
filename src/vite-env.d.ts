b3225bf72a54a0ae0cb3d6f1e4ade625fa20222f
Marzano <arthuramarzano10@gmail.com>
2026-01-08 20:28:46 -0300

    fix(SVGR) corrige erro de sintaxe para usar SVG como compoenente

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
 
 declare module '*.glb';
-declare module '*.png';/// <reference types="vite-plugin-svgr/client" />
+declare module '*.png';
